4266409c7b9bf10459c99a1ba6d8e7e2d406256a
franciscorodriguezsv24 <alejandro.rodriguez@aracaristudios.com>
2025-03-07 00:03:40 -0600

    created my first function

--- tire_volume.py
+++ tire_volume.py
@@ -1,4 +1,14 @@
 ##Hello this is my aplication to calculate the volume of a tire
+import math
+print("Welcome to the Tire Volume Calculator")
+print("Please enter the following measurements in inches.")
 
-name = input("What is your name? ")
-print("Hello, " + name + "!")
+w = float(input("Enter the width of the tire in mm (ex 205):"))
+a = float(input("Enter the aspect ratio of the tire (ex 60):"))
+d = float(input("Enter the diameter of the wheel in inches (ex 15):"))
+
+volume = math.pi * w**2 * a * (w * a + 2540 * d) / 10000000000
+
+volume = round(volume, 2)
+
+print("The approximate volume is: " + str(volume) + " liters")
